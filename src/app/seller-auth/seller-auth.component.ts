37920039f13e24dd7bad1ae235ceb4225c41d983
Sudipto <sbose562@gmail.com>
2023-01-12 23:20:46 +0530

    First commit

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css']
+})
+export class SellerAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  signup(data:object):void{
+    console.warn(data);
+
+  }
+
+}
